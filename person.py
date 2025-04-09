128cebd9718ff5d82cccda05aaae2eb59b91e73b
Hugodan <150121898+Hugodan@users.noreply.github.com>
2025-04-09 11:16:45 +0200

    valueerrors for person attributes

--- person.py
+++ person.py
@@ -1,5 +1,13 @@
 class Person:
     def __init__(self, age, height_m, weight_kg):
+        if height_m < 0 or height_m > 3:
+            raise ValueError("Wrong input")
+        if weight_kg < 0 or weight_kg > 300:
+            raise ValueError("Wrong input")
+        if age < 0 or age > 130:
+            raise ValueError("Wrong input")
+        
+        
         self.age = age
         self.height = height_m
         self.weight = weight_kg
