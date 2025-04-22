29caff2702d66066da2c182a012cd80ea12e340b
Elsa <elsanils@chalmers.se>
2025-04-22 12:02:39 +0200

    database done

---
+++ firebase_config.py
@@ -0,0 +1,25 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# Initialize Firebase with credentials
+cred = credentials.Certificate("firebase-adminsdk.json")
+firebase_admin.initialize_app(cred)
+
+# Get the Firestore client
+db = firestore.client()
+
+def save_user_profile(user_id, age, height, weight, activity, gender):
+    # Reference to the 'users' collection in Firestore
+    user_ref = db.collection('users').document(user_id)
+
+    # Data to store
+    user_data = {
+        'age': age,
+        'height': height,
+        'weight': weight,
+        'activity': activity,
+        'gender': gender,
+    }
+
+    # Set data to Firestore
+    user_ref.set(user_data)
