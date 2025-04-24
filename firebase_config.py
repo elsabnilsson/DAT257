d438d11433c93197cbd6de0dbf7f80479af09dec
Hugodan <150121898+Hugodan@users.noreply.github.com>
2025-04-24 19:33:35 +0200

    sometingwong

--- firebase_config.py
+++ firebase_config.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Initialize Firebase with credentials
-cred = credentials.Certificate("firebase-adminsdk.json")
+cred = credentials.Certificate("DAT257/firebase-adminsdk.json")
 firebase_admin.initialize_app(cred)
 
 # Get the Firestore client
